c2ec11933e6fb39ff2c24610c0dee67091da0934
AnthonyChen0921 <71302562+AnthonyChen0921@users.noreply.github.com>
2022-09-21 22:11:13 -0500

    update name

--- src/components/Item/MealsSummary.js
+++ src/components/Item/MealsSummary.js
@@ -4,7 +4,7 @@ import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>都勾八给爷吃</h2>
+      <h2>My menu</h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
